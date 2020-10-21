df6d603bee39d892c6b88a50315f088db8454336
ko <kyokucho1989@gmail.com>
2020-10-22 05:31:27 +0900

    教材追加

---
+++ ch-keiba.rb
@@ -0,0 +1,17 @@
+require 'pry'
+class Horse
+  attr_reader :name ,:odds
+  def initialize(name)
+    @name = name
+    @odds = (1 + rand(1..50) * 0.1).round(2)
+    puts "名前：#{@name} / オッズ #{@odds}"
+  end
+end
+
+uma1 = Horse.new('タケウマライダー')
+uma2 = Horse.new('ムササビモモンガ')
+uma3 = Horse.new('ジャイロスコープ')
+
+race_horse_list = [uma1,uma2,uma3]
+binding.pry
+p race_horse_list
