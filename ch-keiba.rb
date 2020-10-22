61c947651ab505620c50eadb8455a34c3d29e6a8
ko <kyokucho1989@gmail.com>
2020-10-23 05:40:39 +0900

    教材追加

--- ch-keiba.rb
+++ ch-keiba.rb
@@ -4,7 +4,7 @@ class Horse
   def initialize(name)
     @name = name
     @odds = (1 + rand(1..50) * 0.1).round(2)
-    puts "名前：#{@name} / オッズ #{@odds}"
+    # puts "名前：#{@name} / オッズ #{@odds}"
   end
 end
 
@@ -13,5 +13,15 @@ uma2 = Horse.new('ムササビモモンガ')
 uma3 = Horse.new('ジャイロスコープ')
 
 race_horse_list = [uma1,uma2,uma3]
-binding.pry
-p race_horse_list
+# binding.pry
+# p race_horse_list
+
+# race_horse_list.each do |horse|
+#   puts " #{horse.name} #{horse.odds}"
+
+# end
+
+race_horse_list.each_with_index do |horse,i|
+  puts "No.#{i} #{horse.name} #{horse.odds}"
+
+end
